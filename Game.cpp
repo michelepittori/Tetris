65276f26b6c5008f6e12254f585ad8f16a1eee54
BicuspidNote21 <nicoferro2@gmail.com>
2025-01-22 11:43:41 +0100

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,47 @@
+#include "iostream"
+#include <fstream>
+#include "Game.h"
+using namespace std;
+
+
+
+Game ::Game() : currentTetramino(3,0,'I', false),
+                nextTetramino(3,1,'I',false){
+}
+
+
+int Game::updateScore(int completedLines) {
+    //completedLInes sarebbe un valore da ritornare in una funzione di pulizia delle righe complete
+    int score = completedLines * 100;
+    int bonus = 0;
+    for (int i = 0; i < completedLines; i = i + 1) {
+        bonus = bonus + (i * 100); //se i = 1 bonus = 100, se i = 2 bonus = 200 + 100 ecc..
+    }
+    score += bonus;
+    totalScore = totalScore + score;
+    return totalScore;
+}
+
+void Game::saveScore(const string &fileName, string username, int score) {
+    ofstream outputFile(fileName, ios::app);
+    if (!outputFile) {
+        cout << "Errore nell'apertura del file!" << endl;
+        return;
+    }
+    outputFile << username << " " << score << endl;
+    outputFile.close();
+    cout << "Punteggio salvato con successo!" << endl;
+}
+
+void Game::viewBoard(const std::string &fileName) {
+    //non ordina i punteggi
+    ifstream inputFile;
+    inputFile.open(fileName);
+    string line;
+
+    while(!inputFile.eof()){
+        getline(inputFile,line);
+        cout << line << endl;
+    }
+    inputFile.close();
+}
