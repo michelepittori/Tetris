6320c429924de57f2dfdd263f7c5a8acfa2fba39
BicuspidNote21 <nicoferro2@gmail.com>
2025-01-28 11:25:27 +0100

    Tetramino.h

---
+++ Tetramino.h
@@ -0,0 +1,28 @@
+#ifndef ILTETRIS_TETRAMINO_H
+#define ILTETRIS_TETRAMINO_H
+
+class Tetramino {
+protected:
+    int shape[4][4];            //Matrice per la forma del Tetramino
+    char type;                 // Tipo del Tetramino (I,L,O,T,Z)
+    int x;
+    int y;
+    bool isMirrored;
+
+public:
+    Tetramino();
+    int getX() const;
+    int getY() const;
+    void move(int dx, int dy);
+    void moveRight(Tetramino& tetramino);
+    void moveLeft(Tetramino& tetramino);
+    void moveDown(Tetramino &tetramino);
+    void rotateClockwise();
+    void rotateCounterClockwise();
+    void initializeShape(bool isMirrored);
+    void draw();
+    static int getColor(char type);
+    bool placeTetramino(const Tetramino &tetramino);
+};
+
+#endif //ILTETRIS_TETRAMINO_H
