5ed52c6855c4239c1f158adf1576886b36331bfc
BicuspidNote21 <nicoferro2@gmail.com>
2025-01-28 11:27:35 +0100

    Grid.h

---
+++ Grid.h
@@ -0,0 +1,17 @@
+#ifndef ILTETRIS_GRID_H
+#define ILTETRIS_GRID_H
+#include "Tetramino.h"
+
+class Grid {
+protected:
+    int GRID_HEIGHT;
+    int GRID_WIDTH;
+
+public:
+    Grid();
+    void draw_grid(int start_y, int start_x);
+    bool isCollision(Tetramino& tetramino);
+    void placeTetramino(Tetramino& tetramino);
+};
+
+#endif //ILTETRIS_GRID_H
